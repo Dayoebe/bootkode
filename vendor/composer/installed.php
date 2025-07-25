48e8b3b05e2bf0f9ed583b8aec20d9d863c97811
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-07-25 22:43:29 +0100

    Installed Breeze

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c727c721302161bbbe20bd12207ffae6f8188e70',
+        'reference' => '41fce7e0b54d35c8dc04d280feb8c934d14adb74',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -376,6 +376,15 @@
                 0 => '*',
             ),
         ),
+        'laravel/breeze' => array(
+            'pretty_version' => 'v2.3.8',
+            'version' => '2.3.8.0',
+            'reference' => '1a29c5792818bd4cddf70b5f743a227e02fbcfcd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/breeze',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
         'laravel/framework' => array(
             'pretty_version' => 'v12.21.0',
             'version' => '12.21.0.0',
@@ -388,7 +397,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c727c721302161bbbe20bd12207ffae6f8188e70',
+            'reference' => '41fce7e0b54d35c8dc04d280feb8c934d14adb74',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
