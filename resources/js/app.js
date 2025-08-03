b3cd1095c2d6a01898414ba1da56fa4ac4bd65e3
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-08-03 15:21:55 +0100

    Streamline Alpine.js plugin initialization and comment out unused code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
+Alpine.plugin(persist);
+Alpine.plugin(collapse);
+
 import Alpine from 'alpinejs';
 import persist from '@alpinejs/persist';
 import collapse from '@alpinejs/collapse';
 
-Alpine.plugin(persist);
-Alpine.plugin(collapse);
 
-window.Alpine = Alpine;
-Alpine.start();
+// window.Alpine = Alpine;
+// Alpine.start();
 
 
