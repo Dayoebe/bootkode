44cb2227822456d884a22a459b05fc486e633b13
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-09-11 21:01:03 +0100

    Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4215ee59160ce244095b43037c2d94b02b63b836',
+        'reference' => '285f43fab2085d03025e490af257b48342e7d037',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -496,7 +496,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4215ee59160ce244095b43037c2d94b02b63b836',
+            'reference' => '285f43fab2085d03025e490af257b48342e7d037',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -1015,8 +1015,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
