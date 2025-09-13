fea09fc5b5be631961f36e81ff43f7e4ac407c85
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-09-13 08:42:27 +0100

    Product details

---
+++ app/Livewire/Marketplace/Partial/ProductDetails.php
@@ -0,0 +1,14 @@
+<?php
+
+// app/Livewire/Marketplace/Partial/ProductDetails.php
+namespace App\Livewire\Marketplace\Partial;
+
+use Livewire\Component;
+
+class ProductDetails extends Component
+{
+    public function render()
+    {
+        return view('livewire.marketplace.partial.product-details');
+    }
+}
