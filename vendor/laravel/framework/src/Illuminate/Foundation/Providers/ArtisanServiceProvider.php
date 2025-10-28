0be89dc17a89e5e2d57f9ed8f68e1c37d2266b88
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-10-28 08:27:08 +0100

    clear dependencies

--- vendor/laravel/framework/src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -397,7 +397,9 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
      */
     protected function registerConfigMakeCommand()
     {
-       
+        // $this->app->singleton(ConfigMakeCommand::class, function ($app) {
+        //     return new ConfigMakeCommand($app['files']);
+        // });
     }
 
     /**
