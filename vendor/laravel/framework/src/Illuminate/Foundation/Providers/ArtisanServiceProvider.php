4edea6d930092709146a3331f912a96322006da1
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-10-28 08:20:01 +0100

    Removed ConfigMakeCommand

--- vendor/laravel/framework/src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -39,7 +39,7 @@ use Illuminate\Foundation\Console\ClearCompiledCommand;
 use Illuminate\Foundation\Console\ComponentMakeCommand;
 use Illuminate\Foundation\Console\ConfigCacheCommand;
 use Illuminate\Foundation\Console\ConfigClearCommand;
-use Illuminate\Foundation\Console\ConfigMakeCommand;
+// use Illuminate\Foundation\Console\ConfigMakeCommand;
 use Illuminate\Foundation\Console\ConfigPublishCommand;
 use Illuminate\Foundation\Console\ConfigShowCommand;
 use Illuminate\Foundation\Console\ConsoleMakeCommand;
@@ -190,7 +190,7 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
         'ChannelMake' => ChannelMakeCommand::class,
         'ClassMake' => ClassMakeCommand::class,
         'ComponentMake' => ComponentMakeCommand::class,
-        'ConfigMake' => ConfigMakeCommand::class,
+        // 'ConfigMake' => ConfigMakeCommand::class,
         'ConfigPublish' => ConfigPublishCommand::class,
         'ConsoleMake' => ConsoleMakeCommand::class,
         'ControllerMake' => ControllerMakeCommand::class,
@@ -397,9 +397,7 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
      */
     protected function registerConfigMakeCommand()
     {
-        $this->app->singleton(ConfigMakeCommand::class, function ($app) {
-            return new ConfigMakeCommand($app['files']);
-        });
+       
     }
 
     /**
