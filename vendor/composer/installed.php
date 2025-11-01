8d4cdd7cefff8bfda3de1f657387f5462557eb7f
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-11-01 21:20:17 +0100

    Made some changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1abb0b4eb83c17d25ce6d29c76d0a29b0bdab9fa',
+        'reference' => 'e940a8cb5ed7772349295759c835056a4d4b8f0e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -505,7 +505,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1abb0b4eb83c17d25ce6d29c76d0a29b0bdab9fa',
+            'reference' => 'e940a8cb5ed7772349295759c835056a4d4b8f0e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
