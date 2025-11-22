52f0506c7831bae4416b871aff07f6283d2491d1
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-11-22 20:24:06 +0100

    Added create user menu

--- config/menu.php
+++ config/menu.php
@@ -41,6 +41,7 @@ return [
             'children' => [
                 ['label' => 'User Activity', 'icon' => 'fas fa-history', 'route_name' => 'user.activity', 'roles' => [User::ROLE_SUPER_ADMIN], 'link_id' => 'user.activity',],
                 ['label' => 'Edit User', 'icon' => 'fas fa-user-edit', 'route_name' => 'user-management', 'roles' => [User::ROLE_SUPER_ADMIN, User::ROLE_ACADEMY_ADMIN], 'link_id' => 'user.edit',],
+                ['label' => 'Create User', 'icon' => 'fas fa-user', 'route_name' => 'user-management', 'roles' => [User::ROLE_SUPER_ADMIN, User::ROLE_ACADEMY_ADMIN], 'link_id' => 'user.create',],
                 ['label' => 'All Users', 'icon' => 'fas fa-user-friends', 'route_name' => 'all-users', 'roles' => [User::ROLE_SUPER_ADMIN, User::ROLE_ACADEMY_ADMIN], 'link_id' => 'all-users',],
                 ['label' => 'Roles & Permissions', 'icon' => 'fas fa-user-tag', 'route_name' => 'roles-permissions', 'roles' => [User::ROLE_SUPER_ADMIN], 'link_id' => 'roles-permissions',],
                 ['label' => 'Pending Verifications', 'icon' => 'fas fa-user-clock', 'route_name' => 'pending-verifications', 'roles' => [User::ROLE_ACADEMY_ADMIN, User::ROLE_SUPER_ADMIN], 'link_id' => 'pending-verifications',],
